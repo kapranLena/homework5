c1ccf51368aa07a63c94e7ae2004242240c92007
kapranLena <89081656+kapranLena@users.noreply.github.com>
2021-09-16 11:23:27 +0200

    Add files via upload

--- hw5_3.py
+++ hw5_3.py
@@ -1,1 +1,21 @@
 
+x = input('your text')
+with open('config', 'r') as cf:
+     for line in cf:
+         if x in line:
+          print(line)
+          break
+     else:
+         print('False')
+
+y = open('config', 'a')
+new_config = input('your new configur')
+y.writelines(new_config)
+y.close()
+
+with open('config', 'r', encoding='utf-8') as cf:
+    txt = cf.read()
+    print(type(txt))
+    print(txt)
+
+
